fcf40e9ccb8f7beeffcac6defa2b86106c08caa2
WindDogg <1150547471@qq.com>
2020-04-28 14:21:50 +0800

    完善各个阶段的说明

---
+++ src/main/java/com/dj/dynamicprogramming/Collectchange.java
@@ -0,0 +1,14 @@
+package com.dj.dynamicprogramming;
+
+/**
+ * 凑零钱：
+ * 给你 k 种⾯值的硬币，⾯值分别为 c1, c2 ... ck ，每种硬
+ * 币的数量⽆限，再给⼀个总⾦额 amount ，问你最少需要⼏枚硬币凑出这个
+ * ⾦额，如果不可能凑出，算法返回 -1
+ */
+public class Collectchange {
+    public int coinChange(int[] coins,int amount){
+        return -1;
+
+    }
+}
--- src/main/java/com/dj/dynamicprogramming/Fibsl.java
+++ src/main/java/com/dj/dynamicprogramming/Fibsl.java
@@ -1,10 +1,22 @@
 package com.dj.dynamicprogramming;
 
+/**
+ * 以斐波那契数列为例  主要解决动态规划中的重叠子问题
+ */
 public class Fibsl {
+    /*
+    暴力破解，直接递归时间复杂度O（2^n）
+     */
     public int fib(int n){
         if(n==1 || n==2) return 1;
         return fib(n-1)+fib(n-2);
+
     }
+
+    /**
+     * 增加备忘录数组，计算过的存放到数组中，以备调用
+     * 此方法是自顶向下计算,时间复杂度为O（n）
+     */
 
     public int fib2(int n){
         if(n<1) return 0;
@@ -20,9 +32,49 @@ public class Fibsl {
         return vector[n];
     }
 
+    /**
+     * 受方法2启发，另一条思路，自底向上计算
+     * 时间复杂度为O（n）
+     * @param n
+     * @return
+     */
+    public int fib3(int n){
+        int vector[] = new int[n+1];
+        vector[1]=vector[2]=1;
+        for(int i = 3; i<=n; i++){
+            vector[i]=vector[i-1]+vector[i-2];
+        }
+        return vector[n];
+
+    }
+
+    /**
+     * 改良fib3,无需用数组存储所有的状态，只需存储返回值之前的两个状态即可
+     * 时间复杂度 O（1）
+     * @param n
+     * @return
+     */
+
+    public int fib4(int n){
+        if(n==1 || n==2){
+            return 1;
+        }
+        int num1=1,num2=1;
+        for (int i=3;i<=n;i++){
+            int sum =num1+num2;
+            num1=num2;
+            num2=sum;
+        }
+        return num2;
+    }
+
+
+
     public static void main(String[] args) {
         Fibsl fib = new Fibsl();
-        System.out.println(fib.fib2(7));
-        System.out.println(fib.fib2(6));
+        System.out.println(fib.fib(30));
+        System.out.println(fib.fib2(30));
+        System.out.println(fib.fib3(30));
+        System.out.println(fib.fib4(30));
     }
 }
