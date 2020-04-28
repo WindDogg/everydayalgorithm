92edc1461d888dad9aa9f5955f8759146a39f0ab
WindDogg <1150547471@qq.com>
2020-04-28 10:31:13 +0800

    修改

--- src/main/java/com/dj/day01/Fibsl.java
+++ src/main/java/com/dj/day01/Fibsl.java
@@ -10,21 +10,21 @@ public class Fibsl {
 
     public int fib2(int n){
         if(n<1) return 0;
-        Vector vector = new Vector(n+1,0);
-
+        int[] vector = new int[n+1];
         return help(vector,n);
 
     }
 
-    public int help(Vector<Integer> vector,int n){
+    public int help(int[] vector,int n){
         if(n==1 || n==2) return 1;
-        if(vector.get(n)!=0) return  vector.get(n);
-        vector.add(n,help(vector,n-1)+help(vector,n-2));
-        return vector.get(n);
+        if(vector[n]!=0) return  vector[n];
+        vector[n]=help(vector,n-1)+help(vector,n-2);
+        return vector[n];
     }
 
     public static void main(String[] args) {
         Fibsl fib = new Fibsl();
         System.out.println(fib.fib2(7));
+        System.out.println(fib.fib2(6));
     }
 }
